a14e8529ade455d6aaf35dec51642df9204b7cf6
heraud_b <baptiste.heraud@epitech.eu>
2019-09-28 12:11:18 +0200

    Get module

--- epi.py
+++ epi.py
@@ -3,6 +3,8 @@
 
 import sys
 import argparse
+import requests
+
 def createConfig():
     try:
         f = open(".config_mod", "w+")
@@ -25,8 +27,37 @@ def checkConfig():
         sys.exit(-1)
     return autologin
 
-def main():
 
+def getSpace(autologin, modules):
+    for module in modules:
+        url = autologin + "/module/2019/" + module[0] + "/?format=json"
+        try:
+            response = requests.get(url)
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+        else:
+            modulejson = response.json()
+            print (module[0] + " " + module[1] + " %s " % modulejson["max_ins"])
+
+def getModule(autologin):
+    moduleslist = []
+    try:
+        # If the response was successful, no Exception will be raised
+        response = requests.get(autologin + "/course/filter?format=json&preload=1&location%5B%5D=FR&location%5B%5D=FR%2FPAR&course%5B%5D=master%2Fclassic&scolaryear%5B%5D=2019")
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        modules = response.json()
+        nbmobule = modules["items"]
+        print ("load %d modules" %len(modules["items"]))
+
+        for module in modules["items"]:
+            codeinstance = module["code"] + "/" + module["codeinstance"]
+            moduleslist.append([codeinstance, module["title"] , module["credits"]])
+            #print (module["code"] + "/" + module["codeinstance"])
+        return moduleslist
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", action="store_true", default=False)
     try:
@@ -37,8 +68,7 @@ def main():
         autologin = createConfig()
     else:
         autologin = checkConfig()
-    print ("Recuperation des modules depuis l'intra")
-    print("Your auto login is %s" % autologin)
-
+    modules = getModule(autologin)
+    getSpace(autologin, modules)
 if __name__ == "__main__":
     main()
